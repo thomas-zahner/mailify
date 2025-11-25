cdf8ee314eb01b88529502df4863a8953fdc023a
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-11-25 15:25:45 +0100

    Init project structure

---
+++ bin/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = mailify::add(1, 1);
+    println!("Result: {result}");
+}
---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      self,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          rustVersion = "latest";
+          rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+            extensions = [
+              "rust-src" # for rust-analyzer
+              "rust-analyzer" # usable by IDEs like zed-editor
+              "clippy"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              #pkgs.pkg-config
+              rust
+            ];
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            name = "mailify";
+            meta.mainProgram = "bin";
+            src = ./.;
+            cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = ./Cargo.lock;
+            };
+          };
+        }
+      );
+    };
+}
