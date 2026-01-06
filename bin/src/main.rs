978ac94c6456bf20f6a86a2a1e2abe1433e98bba
Thomas Zahner <thomas.zahner@protonmail.ch>
2026-01-06 14:05:47 +0100

    Parallelise requests in binary

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -6,16 +6,30 @@
 use std::env::args;
 
 use mailify_lib::check;
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
     match args().collect::<Vec<_>>().as_slice() {
         [argv0] => eprintln!("Usage: {argv0} [email address]..."),
-        [_argv0, addresses @ ..] => {
-            for address in addresses {
-                println!("{:?}", check(address).await);
-            }
-        }
+        [_argv0, addresses @ ..] => check_all(addresses.to_vec()).await,
         [] => unreachable!("You shouldn't be able to call programs without argv0"),
+    }
+}
+
+/// Check all addresses in parallel
+async fn check_all(addresses: Vec<String>) {
+    let tasks: Vec<_> = addresses
+        .into_iter()
+        .map(|address| {
+            task::spawn(async move {
+                let result = check(&address).await;
+                println!("{address} - {result}");
+            })
+        })
+        .collect();
+
+    for task in tasks {
+        task.await.unwrap();
     }
 }
